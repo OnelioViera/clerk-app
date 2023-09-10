518125e647d5900e289d8fdecba50c5e2871234d
Onelio Viera <ojvwebdesign@gmail.com>
2023-09-10 11:52:13 -0400

    first commit

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <>
       <nav className="bg-blue-700 py-4 px-6 flex items-center justify-between mb-5">
-        <div className="flex items-center">
+        <div className="flex">
           <Link href="/">
             <div className="text-lg uppercase font-bold text-white">
               Clerk App
